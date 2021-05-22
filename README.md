75edbe58169c68105fbaaab8e44e144397047be1
edsont8 <edsont8@hotmail.com>
2021-05-22 08:06:27 -0300

    Project Start

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ExercicioJavaMapPt1
+# Exercicio - Java - Map-Pt1
 Exercício utilizando Java.Util.Map - BootCamp Java Developer - Digital Innovation One
+
+Crie um Map e execute as seguintes operações:
+- Adicione os 26 estados brasileiros no map, onde a sigla será a chave e o nome do estado o valor;
+- Remova o estado de Minas Gerais;
+- Adicione o Distrito Federal;
+- Verifique o tamanho do mapa;
+- Remova o  estado de Mato Grosso do Sul usando o nome;
+- Navegue em todos os registros do map, mostrando no console no seguinte formato: NOME (SIGLA);
+- Verifique se o estado de Santa Catarina existe no map buscando por sua sigla (SC);
+- Verifique se o estado do Maranhão existe no map buscando por seu nome.
---
+++ src/main/java/ExemplosExercicioMap.java
@@ -0,0 +1,46 @@
+import java.util.TreeMap;
+
+public class ExemplosExercicioMap {
+    public static void main(String[] args) {
+
+        //Crie um Map e execute as seguintes operações:
+        TreeMap<String, String> treeEstados = new TreeMap<>();
+
+        //Adicione os 26 estados brasileiros no map, onde a sigla será a chave e o nome do estado o valor;
+        treeEstados.put("AC", "Acre");
+        treeEstados.put("AL", "Alagoas");
+        treeEstados.put("AP", "Amapa");
+        treeEstados.put("AM", "Amazonas");
+        treeEstados.put("BA", "Bahia");
+        treeEstados.put("CE", "Ceara");
+        treeEstados.put("ES", "Espirito Santo");
+        treeEstados.put("GO", "Goias");
+        treeEstados.put("MA", "Maranhao");
+        treeEstados.put("MT", "Mato Grosso");
+        treeEstados.put("MS", "Mato Grosso do Sul");
+        treeEstados.put("MG", "Minas Gerais");
+        treeEstados.put("PA", "Para");
+        treeEstados.put("PB", "Paraiba");
+        treeEstados.put("PR", "Parana");
+        treeEstados.put("PE", "Pernambuco");
+        treeEstados.put("PI", "Piaui");
+        treeEstados.put("RJ", "Rio de Janeiro");
+        treeEstados.put("RN", "Rio Grande do Norte");
+        treeEstados.put("RS", "Rio Grande do Sul");
+        treeEstados.put("RO", "Rondonia");
+        treeEstados.put("RR", "Roraima");
+        treeEstados.put("SC", "Santa Catarina");
+        treeEstados.put("SP", "Sao Paulo");
+        treeEstados.put("SE", "Sergipe");
+        treeEstados.put("TO", "Tocantins");
+
+        //Remova o estado de Minas Gerais;
+        //Adicione o Distrito Federal;
+        //Verifique o tamanho do mapa;
+        //Remova o estado de Mato Grosso do Sul usando o nome;
+        //Navegue em todos os registros do map, mostrando no console no seguinte formato: NOME (SIGLA);
+        //Verifique se o estado de Santa Catarina existe no map buscando por sua sigla (SC);
+        //Verifique se o estado do Maranhão existe no map buscando por seu nome.
+
+    }
+}
